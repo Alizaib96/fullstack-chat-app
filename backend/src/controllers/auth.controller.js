d6bf8e4c9cd8523b7fcddd1cf6c24597052e93c1
Faizy831 <Faizy35@gmail.com>
2025-06-01 12:09:19 +0500

    project setup-backend

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signupController = (req, res) => {
+  res.send("Signup Page");
+};
+export const loginController = (req, res) => {
+  res.send("Login Page");
+};
+export const logoutController = (req, res) => {
+  res.send("Logout Page");
+};
