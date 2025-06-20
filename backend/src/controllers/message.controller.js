69f1cb55790cf456f8b346d8ff651d0eb5793cea
Alizaib96 <azaib8066@gmail.com>
2025-06-20 19:21:23 +0500

    created chat container

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -38,29 +38,32 @@ export const sendMessageController = async (req, res) => {
   try {
     const { text, image } = req.body;
     const { id: receiverId } = req.params;
-    const senderId = req.user_id;
+    const senderId = req.user._id;
 
     let imageUrl;
     if (image) {
-      // Upload the image to Cloudinary
+      // Upload base64 image to cloudinary
       const uploadResponse = await cloudinary.uploader.upload(image);
       imageUrl = uploadResponse.secure_url;
     }
 
     const newMessage = new Message({
-      receiverId,
       senderId,
+      receiverId,
       text,
       image: imageUrl,
     });
 
     await newMessage.save();
 
-    // todo: real-time fuctionality goes here => Socket.io
+    // const receiverSocketId = getReceiverSocketId(receiverId);
+    // if (receiverSocketId) {
+    //   io.to(receiverSocketId).emit("newMessage", newMessage);
+    // }
 
-    res.status(200).json(newMessage);
+    res.status(201).json(newMessage);
   } catch (error) {
-    console.error("Error in sendMessageController:", error.message);
+    console.log("Error in sendMessage controller: ", error.message);
     res.status(500).json({ error: "Internal server error" });
   }
 };
